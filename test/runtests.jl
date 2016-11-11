c61792e0045ff6eff376e219727d16350b54ca12
Christoph Ortner <c.ortner@warwick.ac.uk>
2016-11-11 14:25:36 +0000

    switched gradients in test sets to be computed via ForwardDiff implemented a generalised multi-well (+ preliminary test) switched to using @testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,30 @@ using Base.Test
 
 include("testsets.jl")
 
+@testset "StaticDimerMethod" begin
+
+println("Test with the muller potential")
+println("------------------------------")
 V = MullerPotential()
 x0, v0 = ic_dimer(V, :near)
 E, dE = objective(V)
 
 dimer = StaticDimerMethod(a_trans=0.002, a_rot=0.002, len=1e-3, maxnit=100)
 x, v, log = run!(dimer, E, dE, x0, v0)
+@test log.res_trans[end] <= dimer.tol_trans
+@test log.res_rot[end] <= dimer.tol_rot
+
+
+println("Test with the standard double-well")
+println("-----------------------------------")
+
+V = DoubleWell()
+x0, v0 = ic_dimer(V, :near)
+E, dE = objective(V)
+
+dimer = StaticDimerMethod(a_trans=0.5, a_rot=0.2, len=1e-3, maxnit=100)
+x, v, log = run!(dimer, E, dE, x0, v0)
+@test log.res_trans[end] <= dimer.tol_trans
+@test log.res_rot[end] <= dimer.tol_rot
+
+end
--- test/testsets.jl
+++ test/testsets.jl
@@ -1,7 +1,15 @@
 
 using Parameters
+import ForwardDiff
 
+
+"""
+return the objective and objective gradient functions
+"""
 objective(V) = x->energy(V, x), x->gradient(V, x)
+
+# no need to compute gradients if ForwardDiff does it for us.
+gradient(V, x) = ForwardDiff.gradient(y->energy(V,y), x)
 
 # ============================================================================
 # TEST CASE 1: Müller Potential
@@ -15,12 +23,8 @@ objective(V) = x->energy(V, x), x->gradient(V, x)
    R::Vector{Vector{Float64}} = [ [1,0], [0,0.5], [-0.5,1.5], [-1,1] ]
 end
 
-energy(V::MullerPotential, r) =
-   sum(0.5 * A * exp(dot(r-R, B*(r-R))) for (A, R, B) in zip(V.A, V.R, V.B))
-
-gradient(V::MullerPotential, r) =
-   sum(A * exp(dot(r-R, B*(r-R))) * (B*(r-R)) for (A, R, B) in zip(V.A, V.R, V.B))
-
+energy(V::MullerPotential, r) = sum( 0.5 * A * exp(dot(r-R, B*(r-R)))
+                                     for (A, R, B) in zip(V.A, V.R, V.B) )
 
 function ic_dimer(V::MullerPotential, case=:near)
    if case == :near
@@ -33,3 +37,28 @@ end
 
 
 # ============================================================================
+# TEST CASE 2: DoubleWell
+# ============================================================================
+
+@with_kw type DoubleWell
+   A::Matrix{Float64} = [1.0 0.0; 0.0 1.0]
+end
+
+DoubleWell(c::Float64) = DoubleWell( diagm([1.0, c])  )
+
+fdw(r) = [r[1]^2 - 1.0; r[2:end]]
+
+energy(V::DoubleWell, r) = energy_sq(V, fdw(r))
+energy_sq(V::DoubleWell, s) = 0.5 * dot(s, V.A * s)
+
+function ic_dimer(V::DoubleWell, case=:near)
+   d = size(V.A,1)-1  # extra dimensions
+   dA = diag(V.A)[2:end]
+   if case == :near
+      x0 = [0.7; 0.2 * dA.^(-1/2)]
+      return x0, - x0
+   elseif case ==:far
+      return [0.8; 0.1*dA^(-1/2)], [-1.0, zeros(d)]
+   end
+   error("unknown initial condition")
+end
