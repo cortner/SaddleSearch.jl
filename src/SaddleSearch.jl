1982f1f21deba00d93b9f62309edc884b60ad8c5
Christoph Ortner <c.ortner@warwick.ac.uk>
2017-01-18 12:48:56 +0000

    and forgot to check in some more changes.

--- src/SaddleSearch.jl
+++ src/SaddleSearch.jl
@@ -24,6 +24,26 @@ Base.dot{T}(x, A::UniformScaling{T}, y) = A.λ * dot(x,y)
 Base.dot(x, A::AbstractMatrix, y) = dot(x, A*y)
 
 
+
+"""
+An abstract linear operator representing `P + s * (Pv) ⊗ (Pv)`
+
+Define `*` and `\`, the latter via Sherman-Morrison-Woodbury formula.
+"""
+type PreconSMW{T} <: AbstractMatrix{T}
+   P       # an invertiable N x N matrix (probably spd)
+   v       # a vector of length N
+   Pv      # the vector P * v
+   s::T    # see doc
+   smw::T  # the SMW-factor
+end
+
+PreconSMW(P, v, s) = PreconSMW(P, v, P*v, s, s / (1.0 + s * dot(v, P, v)))
+
+import Base: *, \, size
+(*)(A::PreconSMW, x::AbstractVector) = A.P * x + (A.s * dot(A.Pv, x)) * A.Pv
+(\)(A::PreconSMW, f::AbstractVector) = (A.P \ f) - ((A.smw * dot(A.v, f)) * A.v)
+
 include("linesearch.jl")
 
 include("staticdimer.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -13,13 +13,15 @@ tests = [
    "molecule2d",
 ]
 
-println("Starting `SaddleSearch` tests")
-println("≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡")
-for test in tests
-      runtest(test)
-end
+# println("Starting `SaddleSearch` tests")
+# println("≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡")
+# for test in tests
+#       runtest(test)
+# end
 
 
 # println("Starting `SaddleSearch` Benchmarks")
 # println("≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡")
 # include("dimer_benchmarks.jl")
+
+runtest("molecule2d")
