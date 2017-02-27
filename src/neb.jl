9d09155ec13da9bf132e60c4480593c8d7569945
Stela Makri <S.Makri@warwick.ac.uk>
2017-02-27 19:25:14 +0000

    fixed several bugs in 'neb' and the 'Vacancy string' example

--- src/neb.jl
+++ src/neb.jl
@@ -20,7 +20,7 @@ adjacent nodes. The tangents were evaluated with the upwind scheme.
 """
 @with_kw type NudgedElasticBandMethod
    alpha::Float64
-   k::Vector{Float64}
+   k::Float64
    # ------ shared parameters ------
    tol_res::Float64 = 1e-5
    maxnit::Int = 1000
@@ -49,27 +49,30 @@ function run!{T}(method::NudgedElasticBandMethod, E, dE, x0::Vector{T})
       for nit = 0:maxnit
          P = precon_prep!(P, x)
          # evaluate gradients, and more stuff
-         dE0 = [dE(x[i]) for i=1:length(x)]
-         E0 = [E(x[i]) for i=1:length(x)]
+         N = length(x)
+         dE0 = [dE(x[i]) for i=1:N]
+         E0 = [E(x[i]) for i=1:N]
          numdE += 1
          numE += 1
          # evaluate the tangents
          ΔE0 = [E0[i-1]-E0[i] for i=2:N]
-         index1 = [ΔE0[i]/abs(ΔE0[i]) - ΔE0[i+1]/abs(ΔE0[i+1]) for i=1:N-1]
-         index2 = [E0[i+1]-E0[i-1]/abs(E0[i+1]-E0[i-1]) for i=2:N]
-         Ediffmax = [maximum(abs(ΔE0[i+1]) , abs(ΔE0[i])) for i=1:N-1]
-         Ediffmin = [minimum(abs(ΔE0[i+1]) , abs(ΔE0[i])) for i=1:N-1]
-         f_weight = 0.5*[(1 + index2[i])*Ediffmax[i] + (index2[i] - 1) * Ediffmin[i] for i=1:N-1]
-         b_weight = 0.5*[(1 + index2[i])*Ediffmin[i] + (index2[i] - 1) *     Ediffmax[i] for i=1:N-1]
-         dxds = [(1 - index1[i]) * f_weight[i] * (x[i+1]-x[i]) + (1 + index1[i]) * b_weight[i] * (x[i]-x[i-1]) for i=2:N-1]
+         index1 = [ΔE0[i]/abs(ΔE0[i]) - ΔE0[i+1]/abs(ΔE0[i+1]) for i=1:N-2]
+         index2 = [E0[i+1]-E0[i-1]/abs(E0[i+1]-E0[i-1]) for i=2:N-1]
+         Ediffmax = [max(abs(ΔE0[i+1]) , abs(ΔE0[i])) for i=1:N-2]
+         Ediffmin = [min(abs(ΔE0[i+1]) , abs(ΔE0[i])) for i=1:N-2]
+         f_weight = 0.5*[(1 + index2[i])*Ediffmax[i] + (index2[i] - 1) * Ediffmin[i] for i=1:N-2]
+         b_weight = 0.5*[(1 + index2[i])*Ediffmin[i] + (index2[i] - 1) *     Ediffmax[i] for i=1:N-2]
+
+         dxds = [(x[i+1]-x[i]) for i=2:N-1]
+
+         # dxds = [(1 - index1[i-1]) .* f_weight[i-1] .* (x[i+1]-x[i]) + (1 + index1[i-1]) .* b_weight[i-1] .* (x[i]-x[i-1]) for i=2:N-1]
          dxds ./= [norm(dxds[i]) for i=1:length(dxds)]
          dxds = [ [zeros(dxds[1])]; dxds; [zeros(dxds[1])] ]
 
-         # d²xds² = [(x[i+1]-x[i]) - (x[i]-x[i-1]) for i=2:N-1]
-         # d²xds² = [[zeros(x[1])]; d²xds²; [zeros(x[1])]]
-
          # evaluate the spring force
-         Fk = k*N*N*[(abs(x[i+1]-x[i]) - abs(x[i]-x[i-1])) * dxds[i] for i=2:N-1]
+         Fk = k*N*N*[dot(x[i+1]-x[i] - x[i]-x[i-1],dxds[i]) * dxds[i] for i=2:N-1]
+
+         # Fk = k*N*N*[(norm(x[i+1]-x[i]) - norm(x[i]-x[i-1])) * dxds[i] for i=2:N-1]
          Fk = [[zeros(dxds[1])]; Fk; [zeros(dxds[1])] ]
          dE0⟂ = [dE0[i] - dot(dE0[i],dxds[i])*dxds[i] for i = 1:length(x)]
 
