6890d99b9ce2c5509b5298745de235253bbfd00f
SMakri <S.Makri@warwick.ac.uk>
2017-05-12 03:09:38 +0100

    some further considerations on 'odestring', still at an early stage

--- src/odestring.jl
+++ src/odestring.jl
@@ -43,51 +43,63 @@ function run!{T}(method::StringMethod, E, dE, x0::Vector{T}, t0::Vector{T})
       @printf(" nit |  sup|∇E|_∞   \n")
       @printf("-----|-----------------\n")
    end
-   for nit = 0:maxnit
-      # normalise t
-      P = precon_prep!(P, x)
-      t ./= [sqrt(dot(t[i], P, t[i])) for i=1:length(x)]
-      t[1] =zeros(t[1]); t[end]=zeros(t[1])
 
-      # ode step
-      # αout, xout = ode45((α_,x_) -> forces(x_, x, dE, P), dofs(path), [.0, alpha]; abstol = 1e-4, reltol = 1e-5 )
-      αout, xout = bs23((α_,x_) -> forces(x_, x, dE, P), [.0, alpha], dofs(path); g = xdof -> reparametrise(x, xdof, P), abstol = 1e-4, reltol = 1e-5 )
+   αout, xout = bs23((α_,x_) -> forces(x_, x, dE, P), [.0, alpha], dofs(path); g = x_ -> reparametrise(x_, x, P), abstol = 1e-4, reltol = 1e-5 )
 
-      numdE += length(x)*length(xout)
-      # residual, store history
-      maxres = maximum([norm(dofs( forces(xout[end], x, dE, P) )[i] ),Inf) for i = 1:length(x)])
-      push!(log, numE, numdE, maxres)
-      if verbose >= 2
-         @printf("%4d |   %1.2e\n", nit, maxres)
-      end
-      if maxres <= tol_res
-         if verbose >= 1
-            println("ODEStringMethod terminates succesfully after $(nit) iterations")
-         end
-         return x, log
-      end
-      # reparametrise
-      ds = [sqrt(dot(x[i+1]-x[i], P, x[i+1]-x[i])) for i=1:length(x)-1]
-      s = [0; [sum(ds[1:i]) for i in 1:length(ds)]]
-      s /= s[end]; s[end] = 1.
-      S = [Spline1D(s, [x[j][i] for j=1:length(s)], w = ones(length(x)),
-            k = 3, bc = "error") for i=1:length(x[1])]
-      x = [[S[i](s) for i in 1:length(S)] for s in linspace(0., 1.,
-                                                               length(x)) ]
-      t = [[derivative(S[i], s) for i in 1:length(S)] for s in
-                                               linspace(0., 1., length(x)) ]
-   end
+
+   # for nit = 0:maxnit
+   #    # normalise t
+   #    P = precon_prep!(P, x)
+   #    t ./= [sqrt(dot(t[i], P, t[i])) for i=1:length(x)]
+   #    t[1] =zeros(t[1]); t[end]=zeros(t[1])
+   #
+   #    # ode step
+   #    αout, xout = ode45((α_,x_) -> forces(x_, x, dE, P), dofs(path), [.0, alpha]; abstol = 1e-4, reltol = 1e-5 )
+   #
+   #    numdE += length(x)*length(xout)
+   #    # residual, store history
+   #    maxres = maximum([norm(dofs( forces(xout[end], x, dE, P) )[i] ),Inf) for i = 1:length(x)])
+   #    push!(log, numE, numdE, maxres)
+   #    if verbose >= 2
+   #       @printf("%4d |   %1.2e\n", nit, maxres)
+   #    end
+   #    if maxres <= tol_res
+   #       if verbose >= 1
+   #          println("ODEStringMethod terminates succesfully after $(nit) iterations")
+   #       end
+   #       return x, log
+   #    end
+   #    # reparametrise
+   #    ds = [sqrt(dot(x[i+1]-x[i], P, x[i+1]-x[i])) for i=1:length(x)-1]
+   #    s = [0; [sum(ds[1:i]) for i in 1:length(ds)]]
+   #    s /= s[end]; s[end] = 1.
+   #    S = [Spline1D(s, [x[j][i] for j=1:length(s)], w = ones(length(x)),
+   #          k = 3, bc = "error") for i=1:length(x[1])]
+   #    x = [[S[i](s) for i in 1:length(S)] for s in linspace(0., 1.,
+   #                                                             length(x)) ]
+   #    t = [[derivative(S[i], s) for i in 1:length(S)] for s in
+   #                                             linspace(0., 1., length(x)) ]
+   # end
    if verbose >= 1
       println("ODEStringMethod terminated unsuccesfully after $(maxnit) iterations.")
    end
    return x, log
 end
 
-function forces{T}(x::Vector{T}, t::Vector{T}, xdof::Vector{Float64}, dE, P)
+function forces{T}(x::Vector{T}, xdof::Vector{Float64}, dE, P)
    x = set_dofs!(x, xdof)
+   P = precon_prep!(P, x)
+
+   S = [Spline1D(linspace(0.,1./length(x)), [x[j][i] for j=1:length(x)],
+         w =  ones(length(x)), k = 3, bc = "error") for i=1:length(x[1])]
+   t= [[derivative(S[i], s) for i in 1:length(S)] for s in linspace(0., 1.,length(x))]
+   t ./= [sqrt(dot(t[i], P, t[i])) for i=1:length(x)]
+   t[1] =zeros(t[1]); t[end]=zeros(t[1])
 
    dE0 = [dE(x[i]) for i=1:length(x)]
    dE0perp = [P \ dE0[i] - dot(dE0[i],t[i])*t[i] for i = 1:length(x)]
+
+   numdE += length(x)
 
    return dofs(- dE0perp)
 
@@ -115,8 +127,7 @@ function reparametrise{T}(x::Vector{T}, xdof::Vector{Float64}, P)
   s /= s[end]; s[end] = 1.
   S = [Spline1D(s, [x[j][i] for j=1:length(s)], w = ones(length(x)),
         k = 3, bc = "error") for i=1:length(x[1])]
-  x = [[S[i](s) for i in 1:length(S)] for s in linspace(0., 1.,
-                                                           length(x)) ]
+  x = [[S[i](s) for i in 1:length(S)] for s in linspace(0., 1., length(x)) ]
   # t = [[derivative(S[i], s) for i in 1:length(S)] for s in
   #                                          linspace(0., 1., length(x)) ]
   return dofs(x)
