863cfc4ccaa2f4ec37b9e98683965c8e7191600e
Stela Makri <S.Makri@warwick.ac.uk>
2017-08-07 19:39:13 +0100

    removed the temporary alterations made in 'string'

--- src/string.jl
+++ src/string.jl
@@ -28,7 +28,7 @@ step-size with an intermediate redistribution of the nodes.
 end
 
 
-function run!{T}(method::StringMethod, E, dE, x0::Vector{T}, t0::Vector{T}, preview::Function)
+function run!{T}(method::StringMethod, E, dE, x0::Vector{T}, t0::Vector{T})
    # read all the parameters
    @unpack alpha, tol_res, maxnit,
             precon_prep!, verbose, precon_cond = method
@@ -43,7 +43,6 @@ function run!{T}(method::StringMethod, E, dE, x0::Vector{T}, t0::Vector{T}, prev
       @printf(" nit |  sup|∇E|_∞   \n")
       @printf("-----|-----------------\n")
    end
-   p=[]
    for nit = 0:maxnit
       # normalise t
       P = precon_prep!(P, x)
@@ -58,7 +57,6 @@ function run!{T}(method::StringMethod, E, dE, x0::Vector{T}, t0::Vector{T}, prev
       push!(log, numE, numdE, maxres)
       if verbose >= 2
          @printf("%4d |   %1.2e\n", nit, maxres)
-         push!(p,preview(x))
       end
       if maxres <= tol_res
          if verbose >= 1
@@ -76,57 +74,6 @@ function run!{T}(method::StringMethod, E, dE, x0::Vector{T}, t0::Vector{T}, prev
       x = [[S[i](s) for i in 1:length(S)] for s in linspace(0., 1., length(x)) ]
       t = [[derivative(S[i], s) for i in 1:length(S)] for s in
       linspace(0., 1., length(x)) ]
-   end
-   if verbose >= 1
-      println("StringMethod terminated unsuccesfully after $(maxnit) iterations.")
-   end
-   return x, log, p
-end
-
-function run2!{T}(method::StringMethod, E, dE, x0::Vector{T}, t0::Vector{T})
-   # read all the parameters
-   @unpack alpha, tol_res, maxnit,
-            precon_prep!, verbose, precon_cond = method
-   P=method.precon
-   # initialise variables
-   x, t = copy(x0), copy(t0)
-   nit = 0
-   numdE, numE = 0, 0
-   log = PathLog()
-   # and just start looping
-   if verbose >= 2
-      @printf(" nit |  sup|∇E|_∞   \n")
-      @printf("-----|-----------------\n")
-   end
-   for nit = 0:maxnit
-      # normalise t
-      P = precon_prep!(P, x)
-      t ./= [sqrt(dot(t[i], P, t[i])) for i=1:length(x)]
-      t[1] =zeros(t[1]); t[end]=zeros(t[1])
-      # evaluate gradients
-      dE0 = [dE(x[i]) for i=1:length(x)]
-      dE0⟂ = [P \ dE0[i] - dot(dE0[i],t[i])*t[i] for i = 1:length(x)]
-      numdE += length(x)
-      # residual, store history
-      maxres = maximum([norm(P*dE0⟂[i],Inf) for i = 1:length(x)])
-      push!(log, numE, numdE, maxres)
-      if verbose >= 2
-         @printf("%4d |   %1.2e\n", nit, maxres)
-      end
-      if maxres <= tol_res
-         if verbose >= 1
-            println("StringMethod terminates succesfully after $(nit) iterations")
-         end
-         return x, log
-      end
-      x -= alpha * dE0⟂
-      # reparametrise
-      ds = [sqrt(dot(x[i+1]-x[i], P, x[i+1]-x[i])) for i=1:length(x)-1]
-      s = [0; [sum(ds[1:i]) for i in 1:length(ds)]]
-      s /= s[end]; s[end] = 1.
-      S = [Spline1D(s, [x[j][i] for j=1:length(s)], w = ones(length(x)),
-            k = 3, bc = "error") for i=1:length(x[1])]
-      t = [[derivative(S[i], si) for i in 1:length(S)] for si in s ]
    end
    if verbose >= 1
       println("StringMethod terminated unsuccesfully after $(maxnit) iterations.")
