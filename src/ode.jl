35246f866939b03a8188d3f938169646d5bb79e3
Christoph Ortner <c.ortner@warwick.ac.uk>
2018-05-03 13:06:29 +0100

    step1 of ode12r rewrite

--- src/ode.jl
+++ src/ode.jl
@@ -1,206 +1,3 @@
-@with_kw type ode23
-   atol::Float64 = 1e-6
-   rtol::Float64 = 1e-3
-   adapt_rtol::Bool = false
-end
-
-function odesolve(solver::ode23, f, x0::Vector{Float64}, N::Int,
-                  log::IterationLog, method;
-                  g=x->x, tol_res=1e-4, maxnit=100 )
-   @unpack atol, rtol, adapt_rtol = solver
-   @unpack verbose = method
-
-   t0 = 0
-   atol0 = atol
-   rtol0 = rtol
-
-   threshold = atol/rtol
-
-   t = t0
-   x = x0[:]
-
-   tout = []
-   xout = []
-
-   numdE, numE = 0, 0
-
-   # computation of the initial step
-   s1, _ = f(t, x, 0)
-   numdE += N
-   push!(tout, t)
-   push!(xout, x)
-   push!(log, numE, numdE, Rn)
-   if verbose >= 2
-      dt = Dates.format(now(), "HH:MM")
-      @printf("SADDLESEARCH: %s |%4d |   %1.2e\n", dt, 0, Rn)
-   end
-   if Rn <= tol_res
-      if verbose >= 1
-         println("SADDLESEARCH: $(typeof(method)) terminates succesfully after $(nit) iterations")
-      end
-      return tout, xout, log
-   end
-
-   if adapt_rtol; rtol = min(rtol0 * norm(s1, Inf), rtol0); end
-   r = norm(s1./max.(abs(x), threshold),Inf) + realmin(Float64)
-   h = 0.8*rtol^(1/3)/r
-
-   for nit = 1:maxnit
-      hmin = 16*eps(Float64)*abs(t)
-
-      abs(h) < hmin ? h = hmin: h = h
-
-      s3, _ = f(t+h*0.75, x+h*0.75*s2, 2*nit+1)
-      tnew = t + h
-      xnew = x + h * (2*s1 + 3*s2 + 4*s3)./9
-      s4, maxres = f(tnew, xnew)
-      numdE += N*3
-
-      # error estimation
-      e = h*(-5*s1 + 6*s2 + 8*s3 - 9*s4)./72
-      err = norm(e./max.(maximum([abs(x) abs(xnew)],2),threshold),Inf) + realmin(Float64)
-
-      if err <= rtol
-         t = tnew
-         x = xnew
-         x = g(x)
-         push!(tout, t)
-         push!(xout, x)
-         s1 = s4 # Reuse final function value to start new step.
-
-         # atol = min(atol0 * norm(s1), atol0)
-         if adapt_rtol; rtol = min(rtol0 * norm(s1, Inf), rtol0); end
-
-         push!(log, numE, numdE, maxres)
-
-         if verbose >= 2
-            dt = Dates.format(now(), "HH:MM")
-            @printf("SADDLESEARCH: %s |%4d |   %1.2e\n", dt, nit, maxres)
-         end
-         if maxres <= tol_res
-            if verbose >= 1
-               println("SADDLESEARCH: $(typeof(method)) terminates succesfully after $(nit) iterations")
-            end
-            return tout, xout, log
-         end
-      end
-
-      # Compute a new step size.
-      h = h*min(5, 0.8*(rtol/err)^(1/3) / 2)
-      if abs(h) <= hmin
-         warn("SADDLESEARCH: Step size $h too small at t = $t.");
-      end
-   end
-
-   if verbose >= 1
-      println("SADDLESEARCH: $(typeof(method)) terminated unsuccesfully after $(maxnit) iterations.")
-   end
-   return tout, xout, log
-end
-
-
-@with_kw type ode12
-   atol::Float64 = 1e-6
-   rtol::Float64 = 1e-3
-   adapt_rtol::Bool = true
-end
-
-
-function odesolve(solver::ode12, f, x0::Vector{Float64}, N::Int,
-                  log::IterationLog, method;
-                  g=x->x, tol_res=1e-4, maxnit=100 )
-
-   @unpack atol, rtol, adapt_rtol = solver
-   @unpack verbose = method
-
-   t0 = 0
-   atol0 = atol
-   rtol0 = rtol
-
-   threshold = atol/rtol
-
-   t = t0
-   x = x0[:]
-
-   tout = []
-   xout = []
-
-   numdE, numE = 0, 0
-
-   # computation of the initial step
-   s1, Rn = f(t, x, 0)
-   numdE += N
-   push!(tout, t)
-   push!(xout, x)
-   push!(log, numE, numdE, Rn)
-   if verbose >= 2
-      dt = Dates.format(now(), "HH:MM")
-      @printf("SADDLESEARCH: %s |%4d |   %1.2e\n", dt, 0, Rn)
-   end
-   if Rn <= tol_res
-      if verbose >= 1
-         println("SADDLESEARCH: $(typeof(method)) terminates succesfully after $(nit) iterations")
-      end
-      return tout, xout, log
-   end
-
-   if adapt_rtol; rtol = min(rtol0 * norm(s1), rtol0); end
-   r = norm(s1./max.(abs(x),threshold),Inf) + realmin(Float64)
-   h = 0.5 * rtol^(1/2) / r
-
-   for nit = 1:maxnit
-      hmin = 16*eps(Float64)*abs(t)
-
-      abs(h) < hmin ? h = hmin: h = h
-
-      s2, maxres = f(t+h, x+h*s1, nit)
-      tnew = t + h
-      xnew = x + h * s1
-
-      numdE += N
-
-      # error estimation
-      e = 0.5 * h * (s2 - s1)
-      err = norm(e./max.(maximum([abs(x) abs(xnew)],2),threshold),Inf) + realmin(Float64)
-
-      if err <= rtol
-         t = tnew
-         x = xnew
-         x = g(x)
-         push!(tout, t)
-         push!(xout, x)
-         s1 = s2
-         # maxres = vecnorm(s1, Inf)
-
-         if adapt_rtol; rtol = min(rtol0 * norm(s1, Inf), rtol0); end
-
-         push!(log, numE, numdE, maxres)
-
-         if verbose >= 2
-            dt = Dates.format(now(), "HH:MM")
-            @printf("SADDLESEARCH: %s |%4d |   %1.2e\n", dt, nit, maxres)
-         end
-         if maxres <= tol_res
-            if verbose >= 1
-               println("SADDLESEARCH: $(typeof(method)) terminates succesfully after $(nit) iterations")
-            end
-            return tout, xout, log
-         end
-      end
-
-      # Compute a new step size.
-      h = h * min(5, 0.5*sqrt(rtol/err) )
-      if abs(h) <= hmin
-         warn("SADDLESEARCH: Step size $h too small at t = $t.");
-      end
-   end
-
-   if verbose >= 1
-      println("SADDLESEARCH: $(typeof(method)) terminated unsuccesfully after $(maxnit) iterations.")
-   end
-   return tout, xout, log
-end
-
 
 
 @with_kw type ODE12r
@@ -215,38 +12,36 @@ end
 
 
 
-function odesolve(solver::ODE12r, f, x0::Vector{Float64}, N::Int,
+function odesolve(solver::ODE12r, f, x0::Vector{Float64},
                   log::IterationLog, method;
                   g=x->x, tol_res=1e-4, maxnit=100 )
 
    @unpack atol, rtol, C1, C2, hmin, extrapolate = solver
-   @unpack verbose = method
+   @unpack verbose, precon, precon_prep! = method
 
    if verbose>=4
        dt = Dates.format(now(), "d-m-yyyy_HH:MM")
        file = open("log_$(dt).txt", "w")
    end
 
-   t0 = 0.0
-
    threshold = atol/rtol
 
-   t = t0
    x = copy(x0)
+   P = precon_prep!(precon, x)
 
-   tout = []
    xout = []
 
    numdE, numE = 0, 0
 
    # computation of the initial step
-   x = g(x)
-   # push(xout, x) TODO: chck if this is the correct place to move this
-   Fn, Rn = f(t, x, 0)
-   numdE += N
-   push!(tout, t)
+   x = g(x, P)
+   P = precon_prep!(P, x)
+   Fn, Rn, ndE = f(x, P, 0)
+   numdE += ndE
+
    push!(xout, x)
    push!(log, numE, numdE, Rn)
+
    if verbose >= 2
       dt = Dates.format(now(), "HH:MM")
       @printf("SADDLESEARCH: %s |%4d |   %1.2e\n", dt, 0, Rn)
@@ -266,7 +61,7 @@ function odesolve(solver::ODE12r, f, x0::Vector{Float64}, N::Int,
          write(file, strlog)
          close(file)
       end
-      return tout, xout, log
+      return xout, log
    end
 
    r = norm(Fn ./ max.(abs.(x), threshold), Inf) + realmin(Float64)
@@ -275,13 +70,13 @@ function odesolve(solver::ODE12r, f, x0::Vector{Float64}, N::Int,
 
    for nit = 1:maxnit
 
-      tnew = t + h
-      xnew = g(x + h * Fn)   # the redistribution is better done here I think
+      xnew = g(x + h * Fn, P)   # the redistribution is better done here I think
                              # that way it implicitly becomes part of `f`
                              # but it seems to make the evolution slower; need more testing!
-      Fnew, Rnew = f(tnew, xnew, nit)
+      Pnew = precon_prep!(P, xnew)
+      Fnew, Rnew, ndE = f(xnew, Pnew, nit)
 
-      numdE += N
+      numdE += ndE
 
       # error estimation
       e = 0.5 * h * (Fnew - Fn)
@@ -321,12 +116,10 @@ function odesolve(solver::ODE12r, f, x0::Vector{Float64}, N::Int,
       h_err = h * 0.5 * sqrt(rtol/err)
 
       if accept
-         t, x, Fn, Rn = tnew, xnew, Fnew, Rnew
-         push!(tout, t)
+         x, Fn, Rn, P = xnew, Fnew, Rnew, Pnew
+
          push!(xout, x)
          push!(log, numE, numdE, Rn)
-
-         # keeping x = g(x) here technically constitutes a bug
 
          if verbose >= 2
             dt = Dates.format(now(), "HH:MM")
@@ -347,7 +140,7 @@ function odesolve(solver::ODE12r, f, x0::Vector{Float64}, N::Int,
                write(file, strlog)
                close(file)
             end
-            return tout, xout, log
+            return xout, log
          end
 
          # Compute a new step size.
@@ -385,13 +178,13 @@ function odesolve(solver::ODE12r, f, x0::Vector{Float64}, N::Int,
       end
 
       if abs(h) <= hmin
-         warn("SADDLESEARCH: Step size $h too small at t = $t.");
+         warn("SADDLESEARCH: Step size $h too small at nit = $nit.");
          if verbose >= 4
-             strlog = @sprintf("SADDLESEARCH: Step size %s too small at t = %s.\n", "$h", "$t")
+             strlog = @sprintf("SADDLESEARCH: Step size %s too small at nit = %s.\n", "$h", "$nit")
              write(file, strlog)
              close(file)
          end
-         return tout, xout, log
+         return xout, log
       end
    end
 
@@ -403,15 +196,8 @@ function odesolve(solver::ODE12r, f, x0::Vector{Float64}, N::Int,
       write(file, strlog)
    end
 
-   # println("DEBUG")
-   # Fold, _ = f(t, x)
-   # println("  |Fold| = ", norm(Fold, Inf))
-   # for h in (1e-1, 1e-2, 1e-3, 1e-4)
-   #    Fnew, _ = f(t, x + h * Fold)
-   #    println("   |Fnew(h)| = ", norm(Fnew, Inf))
-   # end
    if verbose >=4
       close(file)
    end
-   return tout, xout, log
+   return xout, log
 end
