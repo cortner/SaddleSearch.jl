136dc4bbe0dfc9b34e591047f5e1f450c96e0197
Christoph Ortner <christohortner@gmail.com>
2024-05-26 15:50:19 -0700

    fix runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,8 @@
-using SaddleSearch
-using Base.Test
+using SaddleSearch, Test
 using SaddleSearch: numE, numdE, res_trans, res_rot, maxres
-include("testsets.jl"); using TestSets
-
-function heading1(str)
-   dashes = "-"^length(str)
-   print_with_color(:magenta, dashes); println()
-   print_with_color(:magenta, str, bold=true); println()
-   print_with_color(:magenta, dashes);println()
-end
-
-heading2(str) = print_with_color(:green, str*"\n", bold=true)
-
 
 # change this to 2 to get full information or to 0 to get virtually no information
 verbose=1
-
-
 
 println("≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡")
 println(" Starting `SaddleSearch` tests")
